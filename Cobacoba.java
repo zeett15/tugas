2cf321d08e54676a298db2c52ac228b54379c710
zeett15 <qruzeetaprasna15@gmail.com>
2024-09-09 22:33:44 +0700

    Add files via upload

---
+++ Cobacoba.java
@@ -0,0 +1,12 @@
+/**
+ * Cobacoba
+ */
+public class Cobacoba {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------");
+        System.out.println("Biodata Mahasiswa POLINEMA");
+        System.out.println("--------------------------");
+        System.out.println("Alamat  : Jl. Natai arahan");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tugas
+# coba-coba
